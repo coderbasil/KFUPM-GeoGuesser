3c2837919f289fefe92f585236b22b25f8447235
aw99Kfupm <azooooozwar@hotmail.com>
2025-04-13 22:27:23 +0300

    login add

---
+++ src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    
+    <div className="login-container">
+      
+      <form className="login-form">
+        <div className="name-passClass">
+        <center><h2>Login to GeoGuesser</h2></center>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" placeholder="Enter username" />
+        </div>
+        <div className="name-passClass">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Enter password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
