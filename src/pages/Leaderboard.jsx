28521f5d4ec7cf49fc2d612ded09e4bd62b2312f
lili-BADR-ilil <badraziz1424@hotmail.com>
2025-04-14 20:55:00 +0300

    Leaderboard Added

---
+++ src/pages/Leaderboard.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './pages-css/Leaderboard.css';
+
+const Leaderboard = () => {
+    const startNewGame = () => {
+        alert("Starting a new game!");
+        // Add your game start logic here
+    };
+
+    return (
+        <div>
+            <div>
+                <button className="start-game-btn" onClick={startNewGame}>Start New Game</button>
+            </div>
+            <div className="stage">
+                <div className="stage-front">
+                    Leaderboard
+                </div>
+                <div className="tower third">
+                    <div className="face front"></div>
+                    <div className="face right"></div>
+                    <div className="face top">
+                        <div className="player">Player 3</div>
+                    </div>
+                </div>
+                <div className="tower first">
+                    <div className="face front"></div>
+                    <div className="face top">
+                        <div className="player">Player 1</div>
+                    </div>
+                </div>
+                <div className="tower second">
+                    <div className="face front"></div>
+                    <div className="face left"></div>
+                    <div className="face top">
+                        <div className="player">Player 2</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Leaderboard;
