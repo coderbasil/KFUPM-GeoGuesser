87a4eda8010d2b2fbf83bd6366a2d83f3757f743
lili-BADR-ilil <badraziz1424@hotmail.com>
2025-04-19 21:19:30 +0300

    Leaderboard Added #2

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -2,42 +2,48 @@ import React from 'react';
 import '../pages-css/Leaderboard.css';
 
 const Leaderboard = () => {
-    const startNewGame = () => {
-        alert("Starting a new game!");
-    };
+  const startNewGame = () => {
+    alert("Starting a new game!");
+  };
 
-    return (
-        <div className='leaderboard'>
-            <div>
-                <button className="start-game-btn" onClick={startNewGame}>Start New Game</button>
-            </div>
-            <div className="stage">
-                <div className="stage-front">
-                    Leaderboard
-                </div>
-                <div className="tower third">
-                    <div className="face front"></div>
-                    <div className="face right"></div>
-                    <div className="face top">
-                        <div className="player">Player 3</div>
-                    </div>
-                </div>
-                <div className="tower first">
-                    <div className="face front"></div>
-                    <div className="face top">
-                        <div className="player">Player 1</div>
-                    </div>
-                </div>
-                <div className="tower second">
-                    <div className="face front"></div>
-                    <div className="face left"></div>
-                    <div className="face top">
-                        <div className="player">Player 2</div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <button className="start-game-btn" onClick={startNewGame}>Start New Game</button>
+
+      <div className="score">
+        Your score is ..#
+      </div>
+
+      <div className="stage">
+        <div className="stage-front">
+          Leaderboard
         </div>
-    );
+        
+        <div className="tower third">
+          <div className="face front"></div>
+          <div className="face right"></div>
+          <div className="face top">
+            <div className="player">Player 3</div>
+          </div>
+        </div>
+
+        <div className="tower first">
+          <div className="face front"></div>
+          <div className="face top">
+            <div className="player">Player 1</div>
+          </div>
+        </div>
+
+        <div className="tower second">
+          <div className="face front"></div>
+          <div className="face left"></div>
+          <div className="face top">
+            <div className="player">Player 2</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Leaderboard;
