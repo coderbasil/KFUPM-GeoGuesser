658c98deee0016960bf0c8b718f84a9408ba917a
basil <bsuli1424@gmail.com>
2025-04-25 09:15:39 +0300

    fixing some issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import Landing from './pages/Landing';
-import Login from './pages/Login'
-import Leaderboard from './pages/Leaderboard'
-import Photographer from './pages/Photographer'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import React from "react";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Leaderboard from "./pages/Leaderboard";
+import Photographer from "./pages/Photographer";
+import Admin from "./pages/Admin";
+import GamePage from "./pages/GamePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
+  React.useEffect(() => {
+    const preventZoom = (e) => {
+      if (e.ctrlKey || e.metaKey) {
+        e.preventDefault();
+      }
+    };
+    const preventZoomKeys = (e) => {
+      if ((e.ctrlKey || e.metaKey) && ["+", "-", "=", "0"].includes(e.key)) {
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener("wheel", preventZoom, { passive: false });
+    document.addEventListener("keydown", preventZoomKeys);
+
+    return () => {
+      document.removeEventListener("wheel", preventZoom);
+      document.removeEventListener("keydown", preventZoomKeys);
+    };
+  }, []);
+
   return (
     <Router>
       <Routes>
@@ -15,12 +37,7 @@ const App = () => {
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/photographer" element={<Photographer />} />
         <Route path="/admin" element={<Admin />} />
-        
-        {/* 
-        <Route path="/image" element={<Image />} />
-        <Route path="/admin" element={<Admin />} />
-        />
-         */}
+        <Route path="/gamepage" element={<GamePage />} />
       </Routes>
     </Router>
   );
